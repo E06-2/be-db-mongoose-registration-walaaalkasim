526083cddcdd7d4f1338bad7c945d5599598bc26
walaa.alkasim <walaa.alkasim@hotmail.com>
2022-03-31 14:40:16 +0200

    changes

--- db.js
+++ db.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 
-dotenv.config();
+//console.log(process.env);
+
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
 const DB_HOST = process.env.DB_HOST;
-
-//const url = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
+require('dotenv').config();
+//const url = `mongodb+srv://user1:${DB_PASS}@backend.itgsk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const url =
-	'mongodb+srv://user1:bsg0OlVyHUA9igKW@backend.itgsk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+	'mongodb+srv://backend.itgsk.mongodb.net/test?retryWrites=true&w=majority, { user: process.env.DB_USER, pass: process.env.DB_PASS })';
+//const url =
+//'mongodb+srv://user1:bsg0OlVyHUA9igKW@backend.itgsk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const connectdb = async () => {
 	try {
 		await mongoose.connect(url);
--- server.js
+++ server.js
@@ -1,4 +1,22 @@
 //equire('dotenv').config();
+const express = require('express');
+const app = express();
 const db = require('./db');
-
+const dotenv = require('dotenv');
+app.use(express.json());
+dotenv.config();
+const userRoute = require('./routes/userRoute');
 db();
+//app.use('/user', userRoute);
+
+app.post('/user/register', (req, res) => {
+	console.log('body', req.body);
+	res.send('post is not working');
+});
+
+const port = 8082;
+//const port = process.env.PORT;
+app.get('/test', (req, res) => {
+	res.send('testing : get is working');
+});
+app.listen(port, () => console.log(`server started on port ${port}`));
