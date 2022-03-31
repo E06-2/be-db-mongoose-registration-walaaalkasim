24734c2a63745b58c1fa8a4a71da46d74f0e8b24
walaa.alkasim <walaa.alkasim@hotmail.com>
2022-03-31 14:41:05 +0200

    delete comm

--- db.js
+++ db.js
@@ -1,17 +1,14 @@
 const mongoose = require('mongoose');
 
-//console.log(process.env);
-
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
 const DB_NAME = process.env.DB_NAME;
 const DB_HOST = process.env.DB_HOST;
 require('dotenv').config();
-//const url = `mongodb+srv://user1:${DB_PASS}@backend.itgsk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const url =
 	'mongodb+srv://backend.itgsk.mongodb.net/test?retryWrites=true&w=majority, { user: process.env.DB_USER, pass: process.env.DB_PASS })';
-//const url =
-//'mongodb+srv://user1:bsg0OlVyHUA9igKW@backend.itgsk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 const connectdb = async () => {
 	try {
 		await mongoose.connect(url);
