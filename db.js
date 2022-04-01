f0b8e7131dca61c5212d64605b6ab064d61bc544
walaa.alkasim <walaa.alkasim@hotmail.com>
2022-04-01 10:14:22 +0200

    changes

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 //const url =
 //	'mongodb+srv://backend.itgsk.mongodb.net?retryWrites=true&w=majority,{user:process.env.DB_USER,pass:process.env.DB_PASS,name:process.env.DB_NAME})';
 //const url = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}.itgsk.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
-const url = `mongodb+srv://user1:sJ5DAIlolSgU6tU8@backend.itgsk.mongodb.net/demodb?retryWrites=true&w=majority,{password:process.env.DB_PASS}`;
+const url = `mongodb+srv://user1:oZlR3eb41GdTa8bp@backend.itgsk.mongodb.net/demodb?retryWrites=true&w=majority,{password:process.env.DB_PASS}`;
 
 const connectdb = async () => {
 	try {
